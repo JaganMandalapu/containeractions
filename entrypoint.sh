f894e5a48552c39f54fc3ed4759bc13634c3c58f
JaganMandalapu <jagan.mandalapu@gmail.com>
2021-04-05 10:24:16 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,6 @@
 #echo "Hello $INPUT_MYINPUT"
 #memory=$(cat /proc/meminfo)
 #echo "::set-output name=memory::$memory"
+
+env_val=`echo $INPUT_BRANCH_INPUT | sed 's/refs\/heads\///g'`
+echo "::set-output name=env_val::$env_val"
