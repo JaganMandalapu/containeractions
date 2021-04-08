e70638aeab3d6d7735c7ea61e462a6d3ad037eeb
Jagan Mandalapu <jagan.mandalapu@gmail.com>
2021-04-08 11:02:56 -0400

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-my-env=`echo $INPUT_BRANCH_INPUT | sed 's/refs\/heads\///g' | sed 's/RELEASE_art_//g' | tr '[a-z]' '[A-Z]'`
-echo "::set-output name=env_val::${my-env}"
+envout=`echo $INPUT_BRANCH_INPUT | sed 's/refs\/heads\///g' | sed 's/RELEASE_art_//g' | tr '[a-z]' '[A-Z]'`
+echo "::set-output name=env_val::$envout"
 
-if [ $my-env = 'DEV' ]; then
+if [ $envout = 'DEV' ]; then
   runner='ubuntu-20.04'
 else
   runner='ubuntu-18.04'
